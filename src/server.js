6ebcffbac2ecc4b969145bb24de86da7f55a8385
Federico Fracaro <federicohfracaro@gmail.com>
2022-12-05 17:27:47 -0300

    entrega13

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const routerLog=require('./routes/logRoutes')
+const {engine} = require('express-handlebars')
+
+const app = express()
+
+app.use(express.static('public'));
+app.use('/',routerLog)
+
+app.engine("handlebars",engine())
+app.set("view engine","handlebars")
+app.set("views","./views")
+
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, () => { 
+    console.log(`Servidor Http con Websockets escuchando en el puerto ${server.address().port}`);
+})
+server.on('error', error => console.log(`Error en servidor ${error}`))
